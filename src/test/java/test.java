da697b5181f198452fa762e749a0c8be4c0d2ca1
Parufa <tymek@gft.pl>
2025-12-19 12:00:40 +0100

    Reszta Testów

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -4,9 +4,36 @@ class MainTest {
     @org.junit.jupiter.api.Test
     void przyklad1() {
         org.junit.jupiter.api.Assertions.assertEquals(
-                7,
-                Main.znajdzMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+                5,
+                Main.znajdzMarker("bvwbjplbgvbhsrlpgdmjqwftvncz")
         );
     }
-
+    @org.junit.jupiter.api.Test
+    void przyklad2() {
+        org.junit.jupiter.api.Assertions.assertEquals(
+                6,
+                Main.znajdzMarker("nppdvjthqldpwncqszvftbrmjlhg")
+        );
+    }
+    @org.junit.jupiter.api.Test
+    void przyklad3() {
+        org.junit.jupiter.api.Assertions.assertEquals(
+                10,
+                Main.znajdzMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+        );
+    }
+    @org.junit.jupiter.api.Test
+    void przyklad4() {
+        org.junit.jupiter.api.Assertions.assertEquals(
+                11,
+                Main.znajdzMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+        );
+    }
+    @org.junit.jupiter.api.Test
+    void prostyPrzypadek() {
+        org.junit.jupiter.api.Assertions.assertEquals(
+                4,
+                Main.znajdzMarker("abcd")
+        );
+    }
 }
