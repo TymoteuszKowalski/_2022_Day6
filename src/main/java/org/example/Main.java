d9d3fda6813f733ffc52c10546808feea7dc494e
Parufa <tymek@gft.pl>
2025-12-19 11:19:39 +0100

    Zmiany w main, dodanie input.txt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,2 +1,27 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String input = Files.readString(Path.of("input.txt")).trim();
+        int wynik = znajdzMarker(input);
+        System.out.println(wynik);
+    }
+
+    public static int znajdzMarker(String data) {
+        for (int i = 3; i < data.length(); i++) {
+            String czesc = data.substring(i - 3, i + 1);
+            if (wszystkieRozne(czesc)) {
+                return i + 1;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private static boolean wszystkieRozne(String s) {
+        return true;
+    }
+}
