6e722e0b92f6ab1fd430ccf8ef9df28bf77f420a
Parufa <tymek@gft.pl>
2025-12-19 11:50:34 +0100

    Pierwszy test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,12 @@
-public class test {
+import org.example.Main;
+
+class MainTest {
+    @org.junit.jupiter.api.Test
+    void przyklad1() {
+        org.junit.jupiter.api.Assertions.assertEquals(
+                7,
+                Main.znajdzMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+        );
+    }
+
 }
